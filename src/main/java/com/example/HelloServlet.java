288514bde248fbe007745d5463cbde4bd660bc3c
myself-8816 <pavansai131299@gmail.com>
2026-03-24 12:18:22 +0530

    Add files via upload

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,17 @@
+package com.example;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.getWriter().println("<h1>Hello from Jenkins Deployment Project!</h1>");
+    }
+}
